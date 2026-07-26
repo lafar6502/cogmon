333bc7f9e6046220ddd7e6d8e6eb0a6cfc4b221a
rg <rg@uniapp.no>
2026-07-26 14:57:15 +0200

    service name

--- CogMon.Agent/ProjectInstaller.cs
+++ CogMon.Agent/ProjectInstaller.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.Linq;
-
+using System.Configuration;
 
 namespace CogMon.Agent
 {
@@ -14,6 +14,11 @@ namespace CogMon.Agent
         public ProjectInstaller()
         {
             InitializeComponent();
+            if (ConfigurationManager.AppSettings["ServiceName"] != null)
+            {
+                serviceInstaller1.ServiceName = ConfigurationManager.AppSettings["ServiceName"];
+                serviceInstaller1.DisplayName = "CogMon agent " + ConfigurationManager.AppSettings["ServiceName"];
+            }
         }
     }
 }
